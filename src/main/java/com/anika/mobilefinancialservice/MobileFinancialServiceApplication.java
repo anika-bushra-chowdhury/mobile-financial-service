919ea6eda964a7bcf275ab0cdb284683a6041f97
Anika Bushra Chowdhury <anika.bushra@konasl.com>
2022-07-30 23:18:12 +0600

    [mfs] Initial commit

---
+++ src/main/java/com/anika/mobilefinancialservice/MobileFinancialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anika.mobilefinancialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileFinancialServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileFinancialServiceApplication.class, args);
+    }
+
+}
