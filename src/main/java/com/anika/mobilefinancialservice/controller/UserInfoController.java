9bd56a5a057f97cd293a2f3732b3fc481629b8fc
atiQue <atiqurrahman7465@gmail.com>
2022-07-31 21:23:13 +0600

    [MFS] - packages & UserInfoController skeleton.

---
+++ src/main/java/com/anika/mobilefinancialservice/controller/UserInfoController.java
@@ -0,0 +1,17 @@
+package com.anika.mobilefinancialservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author atiQue
+ * @since 31'Jul 2022 at 9:21 PM
+ */
+
+@RestController
+@RequestMapping(value = "/api/v1/user")
+public class UserInfoController {
+
+    //todo: write the apis here
+
+}
